81938c95bff03dd6feca8b77524916cbb3f477c5
JayrahBautista <201980058@psu.palawan.edu.ph>
2022-02-23 22:31:36 +0800

    inc directory done

---
+++ contacts.php
@@ -0,0 +1,7 @@
+<?php include ('inc/header.php'); ?>
+
+<main role="main" class="inner cover">
+        <h1 class="cover-heading">CONTACT</h1>
+        <p class="lead">If you have any questions or suggestions about Cover, please email us to cover.@gmail.com. Thank You!</p>
+</main>
+<?php include ('inc/footer.php'); ?>
---
+++ features.php
@@ -0,0 +1,16 @@
+<?php include ('inc/header.php'); ?>
+<main role="main" class="inner cover">
+        <h1 class="cover-heading" style="text-align: center;">KEY FEATURES</h1>
+        <hr>
+        <Table class="lead" style="background-image: url('building.jpg');">
+          <tr style="text-align: center;">
+            <th>Create Your Own Background</th>
+            <th>Edit On Your Own</th>
+            <th>You Can Download Images</th>
+          </tr>
+        </Table>
+        <hr>
+      </body>
+</main>
+
+<?php include ('inc/footer.php'); ?>
